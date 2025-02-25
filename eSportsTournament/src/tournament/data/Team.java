48260b4339949fde24ff8a74fe56fbdfb3dd2edd
yassine <yasezzlou@alu.edu.gva.es>
2025-02-25 09:21:51 +0100

    commit 4

---
+++ eSportsTournament/src/tournament/data/Team.java
@@ -0,0 +1,12 @@
+package tournament.data;
+
+public class Team extends Participant{
+    /**
+     * Constructor Participant
+     *
+     * @param name
+     */
+    public Team(String name) {
+        super(name);
+    }
+}
